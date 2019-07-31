147a31e3c9dc6ac4d500f8484b303936dc55f42e
Kevin Harvell <kevharvell@gmail.com>
2019-07-31 11:25:56 -0500

    all specs complete

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Draws squares of sideLength x sideLength
 const DrawSquares = (sideLength) => {
     // calculate pixel side length using 700px container width
-    let pixelSideLength = 700 / sideLength;
+    let pixelSideLength = 600 / sideLength;
     let container = document.getElementById("container");
     for(let i = 0; i < sideLength; i++) {
         // create a row to hold the squares
@@ -32,13 +32,23 @@ const Clear = () => {
     }
 }
 
-const ClearButtonHandler = () => {
+const InitHandlers = () => {
     let clearBtn = document.getElementById("clearBtn");
     clearBtn.addEventListener("click", () => {
         Clear();
     });
+    let sideLengthInput = document.getElementById("sideLength");
+    sideLengthInput.addEventListener("change", (e) => {
+        ResizeGrid(e.target.value);
+    });
+}
+
+const ResizeGrid = (sideLength) => {
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+    DrawSquares(sideLength);
 }
 
 // MAIN 
 DrawSquares(16);
-ClearButtonHandler();
+InitHandlers();
